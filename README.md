f0b26ced23058a37bf1f42fe64f471a7aa5369b1
Petter Skau-Nilsen <petteska@stud.ntnu.no>
2021-09-16 14:49:31 +0200

    Added support for session storage + saving result

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Experiment_data_collector
+This is a web-based application for registering information from a experiment for the experiment of the project performed by Petter Skau-Nilsen during the autumn of 2021.
+
+## How to run:
+Open the file index.html in your favorite browser. Then you are good to go!
+
+## Pages:
+### Welcome page
---
+++ utils.js
@@ -0,0 +1,39 @@
+function init() {
+    sessionStorage.clear();
+}
+
+function store(name,data) {
+    sessionStorage.setItem(name, data);
+}
+
+function get(name) {
+    return sessionStorage.getItem(name);
+    
+}
+
+function savefile(filename, data) {
+    const textToBLOB = new Blob([data], { type: 'text/plain' });
+
+    let newLink = document.createElement("a");
+    newLink.download = filename;
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    }
+    else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+
+    newLink.click(); 
+}
+
+function start_experiment() {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    store("Date",date);
+    store("Time",time);
+}
